7a17c68e77c8ba853e407155f641436bdd4bca71
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2021-02-23 23:15:43 +0900

    Prepare Home

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,10 @@
-import { useState } from 'react';
-import sentences from './data/sentences';
-import Sentence from './Sentence';
+import Home from './Home';
 import './App.css';
 
 export default function App() {
-  const [index, setIndex] = useState(0)
-  const sentence = sentences[index]
-
   return (
     <div className="App">
-      {index < sentences.length ? (
-        <>
-          <Sentence en={sentence.en} ja={sentence.ja} />
-          <button className="btn btn-primary" onClick={() => setIndex(index + 1)}>Next</button>
-        </>
-      ) : (
-        <>
-          <div className="alert alert-info">Done!</div>
-          <button className="btn btn-primary" onClick={() => setIndex(0)}>Retry</button>
-        </>
-      )}
+      <Home />
     </div>
   )
 }
---
+++ my-app/src/Play.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import sentences from './data/sentences';
+import Sentence from './Sentence';
+
+export default function Play() {
+  const [index, setIndex] = useState(0)
+  const sentence = sentences[index]
+
+  return (
+    <div className="Play">
+      {index < sentences.length ? (
+        <>
+          <Sentence en={sentence.en} ja={sentence.ja} />
+          <button className="btn btn-primary" onClick={() => setIndex(index + 1)}>Next</button>
+        </>
+      ) : (
+        <>
+          <div className="alert alert-info">Done!</div>
+          <button className="btn btn-primary" onClick={() => setIndex(0)}>Retry</button>
+        </>
+      )}
+    </div>
+  )
+}
